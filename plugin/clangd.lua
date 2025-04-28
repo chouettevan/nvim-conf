bf7e3bcce1a0c76fff86ace8067be8fbcf00a968
chouettevan <pybots743@gmail.com>
2025-04-28 17:54:48 -0400

    added latex

--- plugin/clangd.lua
+++ plugin/clangd.lua
@@ -1,11 +1,18 @@
 local function start_clangd()
         vim.keymap.set('n','<leader>r',':terminal gcc % && ./a.out <CR>')
+        local root_files = {'Makefile'}
+        local paths = vim.fs.find(root_files,{stop = vim.env.HOME})
+        local root_dir = vim.fs.dirname(paths[1])
+        if root_dir == nil then
+            root_dir = vim.fn.getcwd()
+        end
+        print(root_dir)
         vim.lsp.start({
-                name = 'clangd',
-                cmd = {'clangd'}
+            name = 'clangd',
+            cmd = {'clangd'},
+            root_dir = root_dir
         })   
 end
-
 vim.api.nvim_create_autocmd('FileType',{
         pattern = {'c','cpp'},
         desc = 'Start C/C++ LSP',
--- plugin/texlab.lua
+++ plugin/texlab.lua
@@ -1,6 +1,14 @@
 local function start_texlab()
-        vim.keymap.set('n','<leader>r',':!lualatex % <CR>')
-        vim.keymap.set('n','<leader>c',':!rm *.pdf *.aux *.log <CR>')
+        vim.keymap.set('n','<leader>r',':! lualatex % <CR>')
+        vim.keymap.set('n','<leader>b',function()
+            local line_num = tonumber(vim.fn.getpos('.')[2])
+            local col_num = tonumber(vim.fn.getpos('.')[3])
+            local cmd = ':' .. tostring(line_num) .. 'copy '  
+            cmd = cmd .. ' ' .. tostring(line_num) .. ' <CR>'
+            vim.cmd(cmd)
+            vim.cmd(':' .. tostring(line_num+1) .. 's/begin/end/')
+            vim.fn.cursor(line_num,col_num)
+        end)
         vim.lsp.start({
                 name = 'texlab',
                 cmd = {'texlab'},
