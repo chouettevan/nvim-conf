c6d53c9e6207f4a2949ce45d0a97635e7a661f33
chouettevan <pybots743@gmail.com>
2025-06-05 15:35:01 -0400

    added proper directory changes

--- plugin/fsremaps.lua
+++ plugin/fsremaps.lua
@@ -1,12 +1,8 @@
-
-
 vim.g.netrw_bufsettings = 'nu rnu'
-vim.api.nvim_create_autocmd('Filetype',{
-    pattern = 'netrw',
-    callback = function()
-        vim.keymap.set('n','<leader>d','D')
-    end
+vim.g.netrw_keepdir = 0
+vim.api.nvim_create_autocmd('Filetype', {
+  pattern = 'netrw',
+  callback = function()
+    vim.keymap.set('n', '<leader>d', 'D')
+  end
 })
-
-
-
